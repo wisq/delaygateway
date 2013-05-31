efb2f7bd839dca14c3cea45e54be01e827e39845
Adrian Irving-Beer <wisq@wisq.net>
2013-05-31 12:45:28 -0400

    Move to chef11.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :user, "deploy"
 set :use_sudo, false
 
 set :scm, :git
-set :repository, "git@github.com:wisq/delaygateway.git"
+set :repository, "git://github.com/wisq/delaygateway.git"
 set :branch, 'master'
 set :git_shallow_clone, 1
 
@@ -15,15 +15,7 @@ role :app, "delaygateway.ec2.shopify.com"
 set :deploy_via, :remote_cache
 
 namespace :deploy do
-  task :start, :roles => :app do
-    run "sudo sv start /etc/service/delay-*"
-  end
-
-  task :stop, :roles => :app do
-    run "sudo sv stop /etc/service/delay-*"
-  end
-
   task :restart, :roles => :app do
-    run "sudo sv restart /etc/service/delay-*"
+    run "sv-sudo restart /etc/sv/delaygateway-*"
   end
 end
